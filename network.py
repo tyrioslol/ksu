0ff67f6ad9e1fee8fa8a7fc691fe55c4ea502139
tyrioslol <65414969+tyrioslol@users.noreply.github.com>
2021-02-18 15:39:04 -0500

    Update network.py

--- network.py
+++ network.py
@@ -35,7 +35,7 @@ if _os == "debian":
         ans = input()
         if ans == "y":
             print("Updating...")
-            os.system("sudo apt-get update && sudo apt-get upgrade -y")
+            os.system("apt-get update && apt-get upgrade -y")
             break
         if ans == "n":
             print("Moving on...")
@@ -61,31 +61,31 @@ elif _os == "cent":
 if _os == "debian":
     # Install git
     print("\nInstalling git...")
-    os.system("sudo apt-get install git | 2>/dev/null")
+    os.system("apt-get install git -y | 2>/dev/null")
     print("Done")
     os.system("which git")
 
     # Install wget
     print("\nInstalling wget...")
-    os.system("sudo apt-get install wget | 2>/dev/null")
+    os.system("apt-get install wget -y | 2>/dev/null")
     print("Done")
     os.system("which wget")
 
     # Install python3
     print("\nInstalling python...")
-    os.system("sudo apt-get install python3 | 2>/dev/null")
+    os.system("apt-get install python3 -y | 2>/dev/null")
     print("Done")
     os.system("which python3")
 
     # Install UFW
     print("\nInstalling UFW...")
-    os.system("sudo apt-get install ufw | 2>/dev/null")
+    os.system("apt-get install ufw -y | 2>/dev/null")
     print("Done")
     os.system("which ufw")
 
     # Installing nmap
     print("\nInstalling nmap...")
-    os.system("sudo apt-get install nmap | 2>/dev/null")
+    os.system("apt-get install nmap -y | 2>/dev/null")
     print("Done")
     os.system("which nmap")
     print("\nScanning...")
@@ -94,44 +94,44 @@ if _os == "debian":
 
     # Install netstat/nettools
     print("\nInstalling net-tools/netstat...")
-    os.system("sudo apt-get install net-tools | 2>/dev/null")
+    os.system("apt-get install net-tools | 2>/dev/null")
     print("Done")
     os.system("which netstat")
-    os.system("sudo netstat -tulpn | grep LISTEN > netstat.txt")
+    os.system("netstat -tulpn | grep LISTEN > netstat.txt")
     print("Done, results saved to netstat.txt.")
 
     # Installing fail2ban
     print("\nInstalling fail2ban...")
-    os.system("sudo apt-get install fail2ban | 2>/dev/null")
+    os.system("apt-get install fail2ban -y | 2>/dev/null")
     print("Done")
     os.system("fail2ban-client status")
 
     # Importing linpeas
-    print("\nGetting linpeas and running local scan...")
-    os.system("wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh && chmod +x linpeas.sh | 2>/dev/null")
+    # print("\nGetting linpeas and running local scan...")
+    # os.system("wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh && chmod +x linpeas.sh | 2>/dev/null")
 
 if _os == "cent":
     # Install git
     print("\nInstalling git...")
-    os.system("sudo yum install git | 2>/dev/null")
+    os.system("yum install git | 2>/dev/null")
     print("Done")
     os.system("which git")
 
     # Install wget
     print("\nInstalling wget...")
-    os.system("sudo yum install wget | 2>/dev/null")
+    os.system("yum install wget | 2>/dev/null")
     print("Done")
     os.system("which wget")
 
     # Install python3
     print("\nInstalling python...")
-    os.system("sudo yum install python3 | 2>/dev/null")
+    os.system("yum install python3 | 2>/dev/null")
     print("Done")
     os.system("which python3")
 
     # Installing nmap
     print("\nInstalling nmap...")
-    os.system("sudo yum install nmap | 2>/dev/null")
+    os.system("yum install nmap | 2>/dev/null")
     print("Done")
     os.system("which nmap")
     print("\nScanning...")
@@ -140,35 +140,35 @@ if _os == "cent":
 
     # Install netstat/nettools
     print("\nInstalling net-tools/netstat...")
-    os.system("sudo yum install net-tools | 2>/dev/null")
+    os.system("yum install net-tools | 2>/dev/null")
     print("Done")
     os.system("which netstat")
-    os.system("sudo netstat -tulpn | grep LISTEN > netstat.txt")
+    os.system("netstat -tulpn | grep LISTEN > netstat.txt")
     print("Done, results saved to netstat.txt.")
 
     # Installing fail2ban
     print("\nInstalling fail2ban...")
-    os.system("sudo yum install fail2ban | 2>/dev/null")
+    os.system("yum install fail2ban | 2>/dev/null")
     print("Done")
     os.system("fail2ban-client status")
 
     # Importing linpeas
-    print("\nGetting linpeas and running local scan...")
-    os.system("wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh && chmod +x linpeas.sh | 2>/dev/null")
+    # print("\nGetting linpeas and running local scan...")
+    # os.system("wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh && chmod +x linpeas.sh | 2>/dev/null")
 
-while True:
-    print("\nWould you like to run linpeas now? (y/n)")
-    ans = input()
-    if ans == "y":
-        print("INFO: linpeas is present on your system.\nSimply su into another user and run the script with ./linpeas.sh.")
-        print("Done")
-        break
-    if ans == "n":
-        print("Moving on...")
-        break
-    if ans != "y" or ans != "n":
-        print("Invalid: Must be y/n")
-        continue
+#while True:
+#    print("\nWould you like to run linpeas now? (y/n)")
+#    ans = input()
+#    if ans == "y":
+#        print("INFO: linpeas is present on your system.\nSimply su into another user and run the script with ./linpeas.sh.")
+#        print("Done")
+#        break
+#    if ans == "n":
+#        print("Moving on...")
+#        break
+#    if ans != "y" or ans != "n":
+#        print("Invalid: Must be y/n")
+#        continue
         
 while True:
     print("\nIt's recommended to reboot after updating. Would you like to reboot now? (y/n)")
